550fb6c17bb127ce23ee1c106f246aa4fc0867ed
Philip Rasmusson <rasmussonphilip@gmail.com>
2021-02-27 13:43:43 +0100

    Add project files.

---
+++ InsultAPI/Database/StaticData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InsultAPI
+{
+    class StaticData
+    {
+        StaticData()
+        {
+            var adjectiveList = GenerateList("adjective");
+            var amountList = GenerateList("amount");
+            var animalList = GenerateList("animal");
+            var animalPartsList = GenerateList("anmial-parts");
+        }
+
+        private static List<string> GenerateList(string filename)
+        {
+            var list = new List<string>();
+
+
+
+            return list;
+        }
+    }
+
+
+}
