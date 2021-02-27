68461c1545f87a6aabe0c31eddbb03f14480f8fc
Philip Rasmusson <rasmussonphilip@gmail.com>
2021-02-27 17:46:17 +0100

    Finished program for insults

--- InsultAPI/Controller/Generator.cs
+++ InsultAPI/Controller/Generator.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace InsultAPI
 {
-    class Generator
+    public class Generator
     {
+        public static List<string> GenerateList(string filename)
+        {
+            var list = new List<string>();
+            var file = File.ReadLines(@"..\..\..\Database\Words\" + filename + ".txt");
+
+            foreach (var item in file)
+            {
+                list.Add(item);
+            }
+            return list;
+        }
     }
 }
--- InsultAPI/Program.cs
+++ InsultAPI/Program.cs
@@ -6,7 +6,17 @@ namespace InsultAPI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var adjectiveList = Generator.GenerateList("adjective");
+            var amountList = Generator.GenerateList("amount");
+            var animalList = Generator.GenerateList("animal");
+            var animalPartsList = Generator.GenerateList("animal_parts");
+
+
+            var rand = new Random();
+
+            Console.WriteLine($"You {adjectiveList[rand.Next(0, adjectiveList.Count -1)]} {amountList[rand.Next(0, amountList.Count - 1)]} of {animalList[rand.Next(0, animalList.Count - 1)]} {animalPartsList[rand.Next(0, animalPartsList.Count - 1)]}");
+
         }
     }
 }
+
