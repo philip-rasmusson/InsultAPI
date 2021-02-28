43b04cf7ffb220c7d68598ebbde4320aa71cc673
Philip Rasmusson <rasmussonphilip@gmail.com>
2021-02-28 09:49:36 +0100

    Removed unnecessary code

--- InsultAPI/View/Output.cs
+++ InsultAPI/View/Output.cs
@@ -4,8 +4,18 @@ using System.Text;
 
 namespace InsultAPI
 {
-    class Output
+    public class Output
     {
-        
+        public static void OutputInsult()
+        {
+            var adjectiveList = Generator.GenerateList("adjective");
+            var amountList = Generator.GenerateList("amount");
+            var animalList = Generator.GenerateList("animal");
+            var animalPartsList = Generator.GenerateList("animal_parts");
+
+            var rand = new Random();
+
+            Console.WriteLine($"You {adjectiveList[rand.Next(0, adjectiveList.Count - 1)]} {amountList[rand.Next(0, amountList.Count - 1)]} of {animalList[rand.Next(0, animalList.Count - 1)]} {animalPartsList[rand.Next(0, animalPartsList.Count - 1)]}");
+        }
     }
 }
